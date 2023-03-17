6f4d506abcf0d02ed19c19e2a95ca016440a8725
safiir <809320935@qq.com>
2023-03-17 18:57:26 +0800

    fix bug

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.19
 
 require (
 	github.com/eatmoreapple/openwechat v1.4.1 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
 	github.com/samber/lo v1.37.0 // indirect
 	github.com/sashabaranov/go-openai v1.5.2 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 )
--- main.go
+++ main.go
@@ -7,13 +7,107 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/samber/lo"
+	"github.com/syndtr/goleveldb/leveldb"
 
 	"github.com/eatmoreapple/openwechat"
 	openai "github.com/sashabaranov/go-openai"
 )
+
+func Makesure[T any](logic func() (T, error)) (T, error) {
+	for {
+		t, err := logic()
+		if err != nil {
+			time.Sleep(time.Millisecond * 100)
+			continue
+		}
+		return t, nil
+	}
+}
+
+func HandleMsgText(client *openai.Client, msg *openwechat.Message) {
+	content := strings.TrimSpace(msg.Content)
+	if len(content) == 0 {
+		return
+	}
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: content,
+				},
+			},
+		},
+	)
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		msg.ReplyText("oops, something went wrong")
+		return
+	}
+	reply := strings.TrimSpace(resp.Choices[0].Message.Content)
+
+	Makesure(func() (*openwechat.SentMessage, error) {
+		return msg.ReplyText(reply)
+	})
+}
+
+func Reply(msg *openwechat.Message, client *openai.Client, db *leveldb.DB) {
+	sender, _ := msg.Sender()
+	if sender.IsSelf() {
+		return
+	}
+	from := sender.UserName
+	flag := fmt.Sprintf("%s_freeze", from)
+	_, err := db.Get([]byte(flag), nil)
+	if err == nil {
+		return
+	}
+	switch msg.MsgType {
+	case openwechat.MsgTypeText:
+		switch msg.Content {
+		case "start":
+			err = db.Delete([]byte(flag), nil)
+			if err != nil {
+				return
+			}
+			Makesure(func() (*openwechat.SentMessage, error) {
+				return msg.ReplyText("received")
+			})
+		case "end":
+			err = db.Put([]byte(flag), []byte("true"), nil)
+			if err != nil {
+				return
+			}
+			Makesure(func() (*openwechat.SentMessage, error) {
+				return msg.ReplyText("received")
+			})
+		default:
+			HandleMsgText(client, msg)
+		}
+	case openwechat.MsgTypeImage:
+	case openwechat.MsgTypeVoice:
+	case openwechat.MsgTypeVerify:
+	case openwechat.MsgTypePossibleFriend:
+	case openwechat.MsgTypeShareCard:
+	case openwechat.MsgTypeVideo:
+	case openwechat.MsgTypeEmoticon:
+	case openwechat.MsgTypeLocation:
+	case openwechat.MsgTypeApp:
+	case openwechat.MsgTypeVoip:
+	case openwechat.MsgTypeVoipNotify:
+	case openwechat.MsgTypeVoipInvite:
+	case openwechat.MsgTypeMicroVideo:
+	case openwechat.MsgTypeSys:
+	case openwechat.MsgTypeRecalled:
+	default:
+	}
+}
 
 func main() {
 	_ = godotenv.Load()
@@ -46,34 +140,18 @@ func main() {
 	client := openai.NewClientWithConfig(config)
 
 	bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式
+	bot.SyncCheckCallback = func(resp openwechat.SyncCheckResponse) {}
+
+	db, err := leveldb.OpenFile("db", nil)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	defer db.Close()
 
 	// 注册消息处理函数
 	bot.MessageHandler = func(msg *openwechat.Message) {
-		if msg.IsText() {
-			content := strings.TrimSpace(msg.Content)
-			if len(content) == 0 {
-				return
-			}
-			resp, err := client.CreateChatCompletion(
-				context.Background(),
-				openai.ChatCompletionRequest{
-					Model: openai.GPT3Dot5Turbo,
-					Messages: []openai.ChatCompletionMessage{
-						{
-							Role:    openai.ChatMessageRoleUser,
-							Content: content,
-						},
-					},
-				},
-			)
-			if err != nil {
-				fmt.Printf("ChatCompletion error: %v\n", err)
-				msg.ReplyText("oops, something went wrong")
-				return
-			}
-			reply := strings.TrimSpace(resp.Choices[0].Message.Content)
-			msg.ReplyText(reply)
-		}
+		go Reply(msg, client, db)
 	}
 	// 注册登陆二维码回调
 	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
